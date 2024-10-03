fabc510138cbb13b9131dd8838c57de0658b28bd
Dario <dario@kara5.mk>
2024-10-03 13:07:54 +0200

    initial push

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//   $('.your-class').slick({
+//     slidesToShow: 5,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     fade: true,
+//     asNavFor: '.slider-nav'
+//   });
+// });
